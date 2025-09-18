666f4c898f4514383b92bfcc3b3df6678cdef3bf
lokesh267272 <sunkaralokesh0@gmail.com>
2025-09-18 15:15:04 +0530

    Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        strongcyan: 'hsl(172, 67%, 45%)',
+        lightBlue: 'hsl(223, 31%, 20%)',
+        drakGrayishBlue: 'hsl(224, 23%, 55%)',
+        grayishBlue: 'hsl(225, 21%, 49%)',
+      }
+    },
+  },
+  plugins: [],
+}
